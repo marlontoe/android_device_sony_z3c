64abffcb1c1e6bbfda5f30e588d6b80b1e8116b9
marlontoe <inclusionrec@googlemail.com>
2015-10-01 12:35:40 +0200

    TWRP
    Change-Id: If2f10af4e67102ad041e865c7171ab57f2c24f0c

--- z3c.mk
+++ z3c.mk
@@ -45,9 +45,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/thermanager.xml:system/etc/thermanager.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/etc/bootrec-device:recovery/root/etc/bootrec-device \
-
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
