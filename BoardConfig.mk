c2d3e03afbd53279ada172fe5a21fba0d821e637
marlontoe <inclusionrec@googlemail.com>
2016-06-10 16:39:02 +0200

    twrp flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,3 +35,30 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 12656242688 # 12656259072 - 16384
 # SELinux
 BOARD_SEPOLICY_DIRS += \
     device/sony/z3c/sepolicy
+
+# TWRP GENERAL
+RECOVERY_VARIANT := twrp
+TW_MAX_BRIGHTNESS := 255
+
+# TWRP UI
+BOARD_HAS_NO_SELECT_BUTTON := true
+TW_NO_SCREEN_BLANK := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+
+# TWRP Storage
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_FUSE_EXFAT := true
+TW_FLASH_FROM_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TARGET_NO_SEPARATE_RECOVERY := true
+
+# TWRP Crypto
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/bootdevice/by-name/userdata"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
